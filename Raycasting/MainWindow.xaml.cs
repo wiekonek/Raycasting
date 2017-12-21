fe80203d15fbd01b10ea84bdffa4c3e19bac3735
Konrad Kruś <wiekonek@gmail.com>
2017-12-21 21:04:04 +0100

    Add project files.

---
+++ Raycasting/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Raycasting
+{
+  public partial class MainWindow
+  {
+    private Timer aTimer;
+    private double playerPositionX = 22, playerPositionY = 12;
+    private double playerDirectionX = -1, playerDirectionY = 0;
+    private double planeX = 0, planeY = 0.66;
+
+    private const int mapWidth = 24;
+    private const int mapHeight = 24;
+    private readonly int[,] worldMap = new int[mapWidth, mapHeight]
+    {
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+      {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+      {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+      {1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+      {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+      {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
+      {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+      {1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+      {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+      {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+      {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+      {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+      {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+      {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+      {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+      {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+      {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+      {1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+      {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+      {1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+      {1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+      {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+    };
+
+    public MainWindow()
+    {
+      InitializeComponent();
+
+      aTimer = new Timer { Interval = 1000/60 };
+      aTimer.Start();
+      aTimer.Elapsed += OnTimedEvent;
+
+      KeyDown += new KeyEventHandler(OnButtonKeyDown);
+      playerPositionX = (int)Width / 2;
+      playerPositionY = (int)Height / 2;
+    }
+
+    private void OnTimedEvent(Object source, ElapsedEventArgs e)
+    {
+      Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Render, new Action(DrawScene));
+    }
+
+    private void DrawScene()
+    {
+      
+    }
+
+    private void OnButtonKeyDown(object sender, KeyEventArgs e)
+    {
+      switch (e.Key)
+      {
+        case Key.Down:
+          break;
+        case Key.Up:
+          break;
+        case Key.Left:
+          break;
+        case Key.Right:
+          break;
+      }
+    }
+
+    private void PutPixel(int x, int y, Brush color)
+    {
+      var rec = new Rectangle();
+      Canvas.SetTop(rec, y);
+      Canvas.SetLeft(rec, x);
+      rec.Width = 2;
+      rec.Height = 2;
+      rec.Fill = color;
+      MainCanvas.Children.Add(rec);
+    }
+  }
+}
